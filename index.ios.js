dfa33ddb29b35007c06262b8f71a77a352e01419
Anthony Guimard <bonjour@anthonyguimard.fr>
2017-05-31 01:05:43 -0400

    Initialize project and add Sample stuff in iOS Index.

---
+++ index.ios.js
@@ -0,0 +1,49 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ * @flow
+ */
+
+import React, { Component } from 'react';
+import {
+  AppRegistry,
+  StyleSheet,
+  Text,
+  Image,
+  View
+} from 'react-native';
+
+export default class steven extends Component {
+  render() {
+    return (
+      <View style={styles.container}>
+        <Image source={require('./images/steven.jpg')} />
+        <Text style={styles.welcome}>
+          Salut, c'est Steven. {"\n"}
+          Je cherche Peter.
+        </Text>
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#F5FCFF',
+  },
+  welcome: {
+    fontSize: 20,
+    textAlign: 'center',
+    margin: 10,
+  },
+  instructions: {
+    textAlign: 'center',
+    color: '#333333',
+    marginBottom: 5,
+  },
+});
+
+AppRegistry.registerComponent('steven', () => steven);
